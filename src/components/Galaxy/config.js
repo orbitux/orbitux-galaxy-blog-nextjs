8bf45f5181ff3dd178bab7b612de76e2fe983c85
root404-ir <mohammadrg360@gmail.com>
2026-07-08 01:42:31 +0330

    create global galaxy configs

---
+++ src/components/Galaxy/config.js
@@ -0,0 +1,10 @@
+export const galaxyConfig = {
+    count: 10000,
+    radius: 5,
+    branches: 5,
+    spin: 1,
+    randomness: 0.3,
+    randomnessPower: 3,
+    insideColor: "#ff66ff",
+    outSideColor: "#3c0366"
+}
