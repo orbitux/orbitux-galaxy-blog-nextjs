3a6888f7a17f3118ca666c5bf1e232a1c6225661
root404-ir <mohammadrg360@gmail.com>
2026-07-12 21:38:42 +0330

    refactor: refactor dustparticle

--- src/components/Galaxy/components/DustParticles.jsx
+++ src/components/Galaxy/components/DustParticles.jsx
@@ -1,38 +1,51 @@
 import { useFrame } from '@react-three/fiber'
 import React, { useMemo, useRef } from 'react'
 import * as THREE from 'three'
+import { dustFragmentShader, dustVertexShader } from '../shaders/dustShader'
+import { generateDust } from '../generators/dustGenerator'
 const DustParticles = () => {
     const ref = useRef()
+    const { positions, scales } = generateDust()
+    useFrame((state, delta) => {
+        if (!ref.current) return
+        material.uniforms.uTime.value = state.clock.elapsedTime
+        ref.current.rotation.y += delta * 0.0025
+        ref.current.rotation.x += delta * 0.0008
+    })
+    const material = useMemo(() => {
+        return new THREE.ShaderMaterial({
+            vertexShader: dustVertexShader,
+            fragmentShader: dustFragmentShader,
+            transparent: true,
+            depthWrite: false,
+            blending: THREE.AdditiveBlending,
+            uniforms: {
+                uTime: {
+                    value: 0
+                }
+            }
+        })
+    }, [])
     const geometry = useMemo(() => {
-        const count = 8000
-        const positions = new Float32Array(count * 3)
-        for (let i = 0; i < count; i++) {
-            const i3 = i * 3
-            positions[i3] = (Math.random() - 0.5) * 40
-            positions[i3 + 1] = (Math.random() - 0.5) * 40
-            positions[i3 + 2] = (Math.random() - 0.5) * 40
-        }
         const geometry = new THREE.BufferGeometry()
-        geometry.setAttribute("position",
-            new THREE.Float32BufferAttribute(positions, 3)
+        geometry.setAttribute(
+            "position",
+            new THREE.BufferAttribute(
+                positions,
+                3
+            )
+        )
+        geometry.setAttribute(
+            "aScale",
+            new THREE.BufferAttribute(
+                scales,
+                1
+            )
         )
         return geometry
     }, [])
-    useFrame((_, delta) => {
-        if (!ref.current) return
-        ref.current.rotation.y += delta * 0.002
-        ref.current.rotation.x += delta * 0.001
-    })
     return (
-        <points>
-            <pointsMaterial
-                color="#d8b4fe"
-                size={0.015}
-                transparent
-                opacity={0.25}
-                depthWrite={false}
-            />
-        </points>
+        <points ref={ref} geometry={geometry} material={material} />
     )
 }
 
