2a44e7e8d589e0a0f390d9993ce6a1326f290216
root404-ir <mohammadrg360@gmail.com>
2026-07-17 22:27:24 +0330

    chore: commented useSearchIndex and search codes

--- src/hooks/useSearchIndex.js
+++ src/hooks/useSearchIndex.js
@@ -1,31 +1,31 @@
-import { useMemo } from "react"
+// import { useMemo } from "react"
 
-export const useFuse = (posts) => {
-    return useMemo(() => {
-        return new Fuse(posts, {
-            includeScore: true,
-            includeMatches: true,
-            threshold: 0.35,
-            ignoreLocation: true,
-            minMatchCharLength: 2,
-            keys: [
-                {
-                    name: "title",
-                    weight: 5
-                },
-                {
-                    name: "description",
-                    weight: 3
-                },
-            ]
-        })
+// export const useFuse = (posts) => {
+//     return useMemo(() => {
+//         return new Fuse(posts, {
+//             includeScore: true,
+//             includeMatches: true,
+//             threshold: 0.35,
+//             ignoreLocation: true,
+//             minMatchCharLength: 2,
+//             keys: [
+//                 {
+//                     name: "title",
+//                     weight: 5
+//                 },
+//                 {
+//                     name: "description",
+//                     weight: 3
+//                 },
+//             ]
+//         })
 
-    }, [posts])
-}
+//     }, [posts])
+// }
 
-export const useResults = (query, fuse) => {
-    return useMemo(() => {
-        if (!query) return []
-        return fuse.search(query)
-    }, [query, fuse])
-}
+// export const useResults = (query, fuse) => {
+//     return useMemo(() => {
+//         if (!query) return []
+//         return fuse.search(query)
+//     }, [query, fuse])
+// }
--- src/lib/search.js
+++ src/lib/search.js
@@ -1,10 +1,10 @@
-import Fuse from "fuse.js";
-import { getAllPosts } from "./server/post";
-import { useMemo } from "react";
-import { useFuse, useResults } from "@/hooks/useSearchIndex";
+// import Fuse from "fuse.js";
+// import { getAllPosts } from "./server/post";
+// import { useMemo } from "react";
+// import { useFuse, useResults } from "@/hooks/useSearchIndex";
 
-export async function createSearchIndex() {
-    const posts = await getAllPosts()
-    const fuse = useFuse(posts)
-    const results = useResults(query, fuse)
-}
+// export async function createSearchIndex() {
+//     const posts = await getAllPosts()
+//     const fuse = useFuse(posts)
+//     const results = useResults(query, fuse)
+// }
