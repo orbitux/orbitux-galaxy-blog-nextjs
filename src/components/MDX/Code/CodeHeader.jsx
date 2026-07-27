b4d77041ccb6c62f669f8e8f0d283dfb8e4ad2bc
root404 <mohammadrg360@gmail.com>
2026-07-28 00:20:13 +0330

    feat: create codeHeader component for show header

---
+++ src/components/MDX/Code/CodeHeader.jsx
@@ -0,0 +1,22 @@
+import { Copy } from 'lucide-react'
+import React from 'react'
+import LanguageBadge from './LanguageBadge'
+import CopyButton from './CopyButton'
+const CodeHeader = ({ language, filename, target }) => {
+
+    return (
+        <div className='flex flex-row-reverse items-center justify-between border-b border-zinc-700 bg-zinc-900 px-4 py-2'>
+            <div className='flex items-center gap-3'>
+                {filename && (
+                    <span className='text-sm text-zinc-300'>
+                        {filename}
+                    </span>
+                )}
+                <LanguageBadge language={language} />
+            </div>
+            <CopyButton target={target} />
+        </div>
+    )
+}
+
+export default CodeHeader
