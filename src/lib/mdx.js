ada1d7d51e20b5550d8070270019c757c10c251c
root404 <mohammadrg360@gmail.com>
2026-07-28 00:18:43 +0330

    refactor: refactor mdx.js

--- src/lib/mdx.js
+++ src/lib/mdx.js
@@ -3,22 +3,31 @@ import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 
+const options = {
+    theme: 'tokyo-night',
+    keepBackground: false,
+    defaultLang: "plaintext",
+    grid:true,
+    tokenMap:{
+        fn:"function"
+    },
+    onVisitLine(node) {
+        if (node.children.length === 0) {
+            node.children = [{ type: "text", value: "" }]
+        }
+    },
+    onVisitHighlightedLine(node) {
+        node.properties.className = ["highlighted"]
+    },
+    onVisitHighlightedWord(node) {
+        node.properties.className = ["word"]
+    }
+}
+
 export const mdxOptions = {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
         rehypeSlug,
-        [
-            rehypeAutolinkHeadings,
-            {
-                behavior: "append"
-            }
-        ],
-        [
-            rehypePrettyCode,
-            {
-                theme: "github-dark-default",
-                keepBackground: false
-            }
-        ]
+        [rehypePrettyCode, options]
     ]
 }
