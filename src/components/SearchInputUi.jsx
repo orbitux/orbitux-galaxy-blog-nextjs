12bbdd05ba8db6aa59fa8fb951815ebb9d364a06
root404 <mohammadrg360@gmail.com>
2026-08-03 22:08:37 +0330

    feat(searchStyle): responsive input ui

--- src/components/SearchInputUi.jsx
+++ src/components/SearchInputUi.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { useOpen } from '@/contexts/OpenDialogContext'
 import React from 'react'
 import { Button } from './ui/button'
@@ -7,12 +8,12 @@ const SearchInputUi = () => {
     const { setOpen } = useOpen()
     return (
         <>
-            <div className='relative'>
+            <div className='relative w-[100px] md:w-[200px] xl:w-64'>
                 <Input type='text'
                     placeholder='جستجو در مقالات...'
-                    className='cursor-pointer w-3xs border-gray-500'
+                    className='cursor-pointer placeholder:text-[10px] xl:placeholder:text-[16px] border-gray-500'
                     onClick={() => setOpen(true)} />
-                <Button variant='ghost' className='absolute left-0'>ctrl + x</Button>
+                <Button variant='ghost' className='hidden xl:inline xl:absolute xl:left-0'>ctrl + x</Button>
             </div>
         </>
     )
