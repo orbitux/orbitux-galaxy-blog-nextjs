43a6c56acd1b904d095743c50e7885088d8a6625
root404 <mohammadrg360@gmail.com>
2026-08-03 22:09:38 +0330

    feat(post): add date post

--- src/components/PostContentRender.jsx
+++ src/components/PostContentRender.jsx
@@ -2,13 +2,17 @@ import { MDXRemote } from "next-mdx-remote/rsc"
 import Prose from "./Prose"
 import { mdxOptions } from "@/lib/mdx";
 import components from "./MDX/components";
+import { JalaliDate } from "./Date";
 
 export default function PostContentRender({ content, post }) {
-    
+
 
     return (
         <Prose>
-            <span>نویسنده : {post.frontMatter.author}</span>
+            <div className="flex justify-between">
+                <span>نویسنده : {post.frontMatter.author}</span>
+                <span className="fanum">تاریخ انتشار پست : {JalaliDate(post.date)}</span>
+            </div>
             <h1 className="text-xl md:text-3xl font-bold mb-2">{post.frontMatter.title}</h1>
             <img id="content" src={post.frontMatter.thumb} loading="lazy" alt={post.frontMatter.title} className="rounded-xl mb-4 object-contain mt-5 mx-auto" width={800} height={400} />
             <MDXRemote source={content} components={components} options={{
