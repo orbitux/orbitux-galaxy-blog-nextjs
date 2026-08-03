e691e2615e8517009a3c46990e62435d9966fe98
root404 <mohammadrg360@gmail.com>
2026-08-03 22:34:47 +0330

    remove: remove searchdialog from header

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react'
 import { getAllPosts } from '@/lib/server/post'
 import BlogClient from '@/components/BlogClient'
-import SearchDialog from '@/components/SearchDialog'
 import SearchClient from '@/components/SearchClient'
 
 const BlogPage = () => {
