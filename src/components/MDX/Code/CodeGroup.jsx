05bcc2cca358168c6da0d970be6b7f12b8b39d7a
root404 <mohammadrg360@gmail.com>
2026-08-08 12:13:38 +0330

    feat(CodeGroup): create CodeGroup component

---
+++ src/components/MDX/Code/CodeGroup.jsx
@@ -0,0 +1,24 @@
+'use client'
+import React, { Children, cloneElement, useState } from 'react'
+import CodeTabs from './CodeTabs'
+
+const CodeGroup = ({ tabs, children }) => {
+    const [activeTab, setActiveTab] = useState(0)
+    const codes = Children.toArray(children)
+    const activeCode = cloneElement(
+        codes[activeTab],
+        {
+            language: tabs[activeTab]?.language,
+            filename: tabs[activeTab]?.filename,
+            title: tabs[activeTab]?.title
+        })
+    const parsedTabs = typeof tabs === "string" ? JSON.parse(tabs) : tabs
+    return (
+        <div className='my-6 overflow-hidden rounded-xl border border-zinc-800 bg-zinc-950'>
+            <CodeTabs activeTab={activeTab} setActiveTab={setActiveTab} parsedTabs={parsedTabs} />
+            {activeCode}
+        </div>
+    )
+}
+
+export default CodeGroup
