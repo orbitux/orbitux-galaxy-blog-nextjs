17f6d7478da88ede76553dc3afc4b19cea3dc570
root404 <mohammadrg360@gmail.com>
2026-08-08 12:13:03 +0330

    reafactor(copy):refactor copy button

--- src/components/MDX/Code/CodeCopyButtons.jsx
+++ src/components/MDX/Code/CodeCopyButtons.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { MultiMaterial } from '@react-three/drei'
 import { Check, Copy } from 'lucide-react'
 import React, { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -6,39 +7,51 @@ import { createRoot } from 'react-dom/client'
 const mounted = new WeakMap()
 const CodeCopyButtons = () => {
     useEffect(() => {
+        const mountButtons = () => {
+            document.querySelectorAll("[data-copy-button]").forEach(button => {
+                if (mounted.has(button)) return
+                const root = createRoot(button)
+                mounted.set(button, root)
+                root.render(
+                    <Copy size={16} />
+                )
+            })
+        }
+        mountButtons()
+        const observer = new MutationObserver(() => {
+            mountButtons()
+        })
+        observer.observe(
+            document.body,
+            {
+                childList: true,
+                subtree: true
+            }
+        )
         const handleClick = async (e) => {
             const button = e.target.closest("[data-copy-button]")
             if (!button) return
             const figure = button.closest("figure")
             const code = figure?.querySelector("pre code")
             if (!code) return
-
             await navigator.clipboard.writeText(code.innerText)
             const root = mounted.get(button)
-            root.render(
-                <Check size={16} className='text-green-400' />
+            root?.render(
+                <Check size={16} className='text-green-500' />
             )
             setTimeout(() => {
-                root.render(
+                root?.render(
                     <Copy size={16} />
                 )
             }, 2000)
         }
+
         document.addEventListener('click', handleClick)
-        document.querySelectorAll("[data-copy-button]").forEach(button => {
-            if (mounted.has(button)) return
-            const root = createRoot(button)
-            mounted.set(button, root)
-            root.render(
-                <Copy size={16} />
-            )
-        })
         return () => {
+            observer.disconnect()
             document.removeEventListener('click', handleClick)
-            mounted.forEach?.(() => { })
         }
     }, [])
-    return null
-}
 
+}
 export default CodeCopyButtons
