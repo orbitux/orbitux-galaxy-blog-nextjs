023da68a7bfe37a37f41bc49a9780fb9b8fc67a1
root404 <mohammadrg360@gmail.com>
2026-08-09 18:17:01 +0330

    feat(style):margin and padding

--- src/components/MDX/Code/CodeBlock.jsx
+++ src/components/MDX/Code/CodeBlock.jsx
@@ -1,16 +1,18 @@
 'use client'
 import React, { useRef } from 'react'
 import CodeHeader from './CodeHeader'
+import CodeTabs from './CodeTabs'
 // import CopyCodeBtn from './CopyCodeBtn'
 
 const CodeBlock = ({ children, language = "text", filename = "" }) => {
     const codeRef = useRef(null)
     return (
-        <figure className='text-left'>
+        <figure className='m-0 p-0 text-left'>
             <CodeHeader filename={filename} language={language} target={codeRef} />
             <div ref={codeRef}>
                 {children}
             </div>
+
         </figure>
     )
 }
