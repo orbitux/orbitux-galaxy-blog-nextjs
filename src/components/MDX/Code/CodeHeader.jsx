0c32069feae301a3acc44cd8765679f7824192e2
root404 <mohammadrg360@gmail.com>
2026-08-09 18:22:59 +0330

    refactor: remove unused tailwind class

--- src/components/MDX/Code/CodeHeader.jsx
+++ src/components/MDX/Code/CodeHeader.jsx
@@ -1,20 +1,19 @@
+'use client'
 import { Copy } from 'lucide-react'
-import React from 'react'
+import React, { cloneElement } from 'react'
 import LanguageBadge from './LanguageBadge'
 import CopyButton from './CopyButton'
+import CodeTabs from './CodeTabs'
 const CodeHeader = ({ language, filename, target }) => {
 
     return (
-        <div className='flex flex-row-reverse items-center justify-between border-b border-zinc-700 bg-zinc-900 px-4 py-2'>
-            <div className='flex items-center gap-3'>
-                {filename && (
-                    <span className='text-sm text-zinc-300'>
-                        {filename}
-                    </span>
-                )}
-                <LanguageBadge language={language} />
-            </div>
-            {/* <CopyButton target={target} /> */}
+        <div className='border-b border-zinc-700 bg-zinc-900 px-4 py-2'>
+            {filename && (
+                <span className='text-sm text-zinc-300'>
+                    {filename}
+                </span>
+            )}
+            <LanguageBadge language={language} />
         </div>
     )
 }
