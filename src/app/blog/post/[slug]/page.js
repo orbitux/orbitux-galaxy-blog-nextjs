5e4e89345f9f1724fe77422fd9ba9d638442271d
root404 <mohammadrg360@gmail.com>
2026-08-31 16:19:00 +0330

    feat: create JSON-Ld

--- src/app/blog/post/[slug]/page.js
+++ src/app/blog/post/[slug]/page.js
@@ -42,8 +42,34 @@ export default async function PostPage({ params }) {
     if (!post) {
         return notFound()
     }
+    const { frontMatter } = post
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "BlogPosting",
+
+        headline: frontMatter.title,
+        description: frontMatter.description,
+
+        image: [`${baseUrl}${frontMatter.thumb}`],
+        author: {
+            "@type": "Person",
+            name: frontMatter.author,
+        },
+        publisher: {
+            "@type": "Person",
+            name: "Orbitux",
+            url: baseUrl
+        },
+        mainEntityOfPage: {
+            "@type": "WebPage",
+            "@id": `${baseUrl}/blog/post/${frontMatter.slug}`
+        }
+    }
     return (
         <div>
+            <script type="application/ld+json" dangerouslySetInnerHTML={{
+                __html: JSON.stringify(jsonLd)
+            }} />
             <PostClient post={post} />
             <CodeCopyButtons />
         </div>
